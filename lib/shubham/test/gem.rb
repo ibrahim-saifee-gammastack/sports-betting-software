b4f1c4e69a1d1f098f6d61d40ba931cbc4ac12b1
Ibrahim <isaifee@grepruby.com>
2020-01-14 15:27:38 +0530

    initial commit

---
+++ lib/shubham/test/gem.rb
@@ -0,0 +1,10 @@
+require "shubham/test/gem/version"
+
+module Shubham
+  module Test
+    module Gem
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/shubham/test/gem/version.rb
@@ -0,0 +1,7 @@
+module Shubham
+  module Test
+    module Gem
+      VERSION = "0.1.1.4"
+    end
+  end
+end
